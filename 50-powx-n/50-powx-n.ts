dd7d3881d6c45b7b560f3106f03398c24d3bced7
Sayed Ayman <sayedayman203@gmail.com>
2022-05-07 12:38:57 +0200

    Time: 126 ms (16.74%), Space: 44.2 MB (41.41%) - LeetHub

--- 50-powx-n/50-powx-n.ts
+++ 50-powx-n/50-powx-n.ts
@@ -1,3 +1,13 @@
 function myPow(x: number, n: number): number {
-    return x**n;
+    if (n === 0) {
+        return 1;
+    } else if (n === 1) {
+        return x;
+    }
+    
+    if (n < 0) {
+        x = 1/x;
+        n *= -1;
+    }
+    return n%2 ===0 ? myPow(x*x, Math.floor(n/2)) : myPow(x*x, Math.floor(n/2)) * x;
 };
