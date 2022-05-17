84ffd65c1b2cd20df3c8c74d4bebcb4f786d020c
Sayed Ayman <sayedayman203@gmail.com>
2022-05-17 14:00:37 -0700

    Time: 152 ms (65.01%), Space: 53.7 MB (25.66%) - LeetHub

--- 189-rotate-array/189-rotate-array.ts
+++ 189-rotate-array/189-rotate-array.ts
@@ -2,7 +2,6 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    for ( let i = 0; i < k; i++) {
-        nums.unshift(nums.pop());
-    }
+    k %= nums.length;
+    nums.unshift(...nums.splice(nums.length -k, k));
 };
